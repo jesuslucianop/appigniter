9508117277346786498fbd6a7692f73dcf8ab774
jesuslucianop <luciano-peralta@hotmail.com>
2017-08-10 00:10:03 -0400

    second commit

--- application/views/Form_cliente.php
+++ application/views/Form_cliente.php
@@ -3,19 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html>
 <head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>Formulario para personas</title>
 </head>
 <body>
 <h1>En este formulario se Registraran Personas</h1>
 
+<div class="row">
+<div class="col-md-6">
+<div class="form-group iput-group">
+<span class="input-group-addon">Nombre</span>
+<input  type="text" name="txtnombre" class="form-control"/>
+
+
+</div>
+<div class="form-group iput-group">
+<span class="input-group-addon">Apellido</span>
+<input type="text" name="txtnombre"  class="form-control"/>
+</div>
+
+</div>
+
+</div>
 <?php
-     echo form_open('\Cliente\index');
-     echo form_label('Nombre', 'nombre');
-     echo form_input('nombre');echo '<br>';
-     echo form_label('Sueldo', 'sueldo');
-     echo form_input('sueldo');echo '<br>';
-     echo form_submit('botonSubmit', 'Enviar');
-     echo form_close();
+
  ?>
 </body>
 
